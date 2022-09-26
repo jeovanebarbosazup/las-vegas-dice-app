07142696190138838530447c7c205c3b40178909
jeovanebarbosazup <jeovane.barbosa@zup.com.br>
2022-09-26 16:56:57 -0300

    Simple Random Dice app

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -9,12 +9,20 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var diceImageOne: UIImageView!
+    @IBOutlet weak var diceImageTwo: UIImageView!
+    
+    let arrayOfDices = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"),UIImage(named: "DiceSix")]
+    
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        
+        var randomLeftDice = Int.random(in: 0...5)
+        diceImageOne.image = arrayOfDices[randomLeftDice]
+        print("The value of the left dice is: \(randomLeftDice + 1)")
+        
+        var randomRightDice = Int.random(in: 0...5)
+        diceImageTwo.image = arrayOfDices[randomRightDice]
+        print("The value of the right dice is: \(randomRightDice + 1)")
     }
-
-
 }
-
